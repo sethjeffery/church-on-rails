00c26c154589881869da8e6cb0fc3e64087ce273
Seth Jeffery <seth@quipper.com>
2016-11-16 08:31:50 +0000

    woops

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,4 +1,4 @@
 class Family < ApplicationRecord
   has_many :family_memberships
-  has_many :people, through: :family_memberships, inverse_of: :family
+  has_many :people, through: :family_memberships, inverse_of: :families
 end
