bc12e824fa5be4160c38b0183dc97bcf151b2f8f
Seth Jeffery <seth@quipper.com>
2016-11-17 13:44:26 +0000

    refactor and add container to flash messages

--- app/helpers/alerts_helper.rb
+++ app/helpers/alerts_helper.rb
@@ -1,33 +1,44 @@
 module AlertsHelper
-  def flash_messages(model=nil)
+  def flash_messages(args={})
     if flash[:alert]
       html = <<-HTML
         <div class="alert alert-danger">
           #{flash[:alert]}
         </div>
       HTML
-      html.html_safe
+
     elsif flash[:info]
       html = <<-HTML
         <div class="alert alert-info">
           #{flash[:info]}
         </div>
       HTML
-      html.html_safe
+
     elsif flash[:notice]
       html = <<-HTML
         <div class="alert alert-info">
           #{flash[:notice]}
         </div>
       HTML
-      html.html_safe
+
     elsif flash[:success]
       html = <<-HTML
         <div class="alert alert-success">
           #{flash[:success]}
         </div>
       HTML
-      html.html_safe
+    else
+      html = ''
+    end
+
+    wrap_message(html, args)
+  end
+
+  def wrap_message(message, args={})
+    if args[:container]
+      "<div class='#{args[:container]}'>#{message}</div>".html_safe
+    else
+      message.html_safe
     end
   end
 end
