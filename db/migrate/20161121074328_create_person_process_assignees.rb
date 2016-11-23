071f906dc8015b4425ccfadfc63933c6eba313d3
Seth Jeffery <seth@quipper.com>
2016-11-23 15:35:23 +0000

    fix up reference

--- db/migrate/20161121074328_create_person_process_assignees.rb
+++ db/migrate/20161121074328_create_person_process_assignees.rb
@@ -1,10 +1,11 @@
 class CreatePersonProcessAssignees < ActiveRecord::Migration[5.0]
   def change
     create_table :person_process_assignees do |t|
-      t.references :assignee, foreign_key: true
+      t.references :assignee, references: :people
       t.references :person_process, foreign_key: true
-
       t.timestamps
     end
+
+    add_foreign_key :person_process_assignees, :people, column: :assignee_id
   end
 end
