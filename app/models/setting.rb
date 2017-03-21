e97b3a18d4ad6dee9b97843b31a1405d16592337
Seth Jeffery <seth@quipper.com>
2017-03-21 16:39:16 +0000

    add more documentation and refactor

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,36 +1,43 @@
+# Handles all site settings with `fetch` and `store` convenience methods.
+# Settings can be stored in the database, but can also be defaulted
+# with ENV variables or in the config/application.yml file.
+#
 class Setting < ApplicationRecord
   validates_uniqueness_of :key
 
+  # Stores a setting in the database.
   def self.store(key, value)
-    write_cache(key, value)
-    write_database(key, value)
+    store_to_cache(key, value)
+    store_to_database(key, value)
   end
 
+  # Reads a setting from the database, or from ENV.
   def self.fetch(key)
-    key = safe_key(key)
-
-    if Rails.cache.exist?("Setting.#{key}")
-      value = Rails.cache.read("Setting.#{key}")
-    else
-      record = where(key: key).first
-      value = record.nil? ? ENV[key] : record.value
-    end
-
-    write_cache(key, value)
-    value
-  end
-
-  def self.safe_key(key)
-    key.to_s.underscore.upcase
+    _fetch(key).tap{|value| write_cache(key, value)}
   end
 
   private
 
-  def self.write_cache(key, value)
+  # Converts a string to UPPER_SNAKECASE for consistency.
+  def self.safe_key(key)
+    key.to_s.underscore.upcase
+  end
+
+  def self.store_to_cache(key, value)
     Rails.cache.write("Setting.#{safe_key(key)}", value)
   end
 
-  def self.write_database(key, value)
+  def self.store_to_database(key, value)
     find_or_initialize_by(key: safe_key(key)).update(value: value)
+  end
+
+  def self._fetch(key)
+    key = safe_key(key)
+    if Rails.cache.exist?("Setting.#{key}")
+      Rails.cache.read("Setting.#{key}")
+    else
+      record = where(key: key).first
+      record.nil? ? ENV[key] : record.value
+    end
   end
 end
