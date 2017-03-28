d3c4a636573dee36556bbe0c97ac74d81ecef5ca
Seth Jeffery <seth@quipper.com>
2017-03-28 14:56:21 +0100

    mailgun support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,18 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  if ENV['MAILGUN_SMTP_PORT']
+    config.action_mailer.smtp_settings = {
+      :port           => ENV['MAILGUN_SMTP_PORT'],
+      :address        => ENV['MAILGUN_SMTP_SERVER'],
+      :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+      :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+      :domain         => Setting.fetch(:host_name),
+      :authentication => :plain,
+    }
+    config.action_mailer.delivery_method = :smtp
+  end
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
