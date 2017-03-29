b12be8694055ee88645130302aa4bf31fd47df90
Seth Jeffery <seth@quipper.com>
2017-03-29 07:59:31 +0100

    don't go over daily geocode limits so quickly

--- app/models/concerns/geocoding.rb
+++ app/models/concerns/geocoding.rb
@@ -5,10 +5,8 @@ module Concerns
     def full_address(with_name: false)
       arr = [address1, address2, postcode, country].select(&:present?)
       arr = [to_s] + arr if with_name
-      if arr.present?
-        arr << 'UK' if country.blank?
-        arr.select(&:present?).join(', ')
-      end
+      arr << 'UK' if arr.present? && country.blank?
+      arr.select(&:present?).join(', ')
     end
 
     def lat
@@ -21,7 +19,13 @@ module Concerns
 
     included do
       geocoded_by :full_address
-      after_validation :geocode, unless: ->{ Rails.env.test? || Geocoder.config.lookup == :test }
+      after_validation :geocode, if: :should_geocode?
+    end
+
+    def should_geocode?
+      return false if Rails.env.test? || Geocoder.config.lookup == :test
+      return false if full_address.blank?
+      lat.blank? || lng.blank? || address1_changed? || address2_changed? || postcode_changed?
     end
   end
 end
